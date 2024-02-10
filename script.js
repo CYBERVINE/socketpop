56c3e401d71070c3dfdb780e36bf7fcf4ddb627e
Kevin <kevinspbyrne@gmail.com>
2024-02-10 13:42:57 -0800

    display remote track on caller side

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const socket = io('https://localhost:8000/')
+// const socket = io('https://192.168.1.72:8000/')
 const localVideo = document.getElementById('localVideo')
 const remoteVideo = document.getElementById('remoteVideo')
 const startGame = document.getElementById('startGame')
@@ -20,39 +21,40 @@ const getMedia = async () => {
     localVideo.srcObject = localStream
 }
 
+const peerConnection = new RTCPeerConnection(configuration)
+
 const createPeerConnection = async (connection) => {
+    localStream.getTracks().forEach(track=>{
+        console.log(track)
+        peerConnection.addTrack(track,localStream)})
+
     console.log(connection)
-    const peerConnection = new RTCPeerConnection(configuration)
     if (connection === undefined){
-        localStream.getTracks().forEach(track=>{
-            console.log(track)
-            peerConnection.addTrack(track,localStream)})
         const offer = await peerConnection.createOffer() // SDP
         localDescription = await peerConnection.setLocalDescription(offer)
         socket.emit('offer', offer)
     } else {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(connection.offer))
-        localStream.getTracks().forEach(track=>{
-            console.log(track)
-            peerConnection.addTrack(track,localStream)})
+        console.log(connection)// use this socket id to get recent offerer
+        await peerConnection.setRemoteDescription(new RTCSessionDescription(connection.offer))
         const answer = await peerConnection.createAnswer() //SDP
         await peerConnection.setLocalDescription(answer)
-        console.log(answer)
+        // remoteVideo.srcObject = peerConnection.MediaStream
+        console.log(connection.tracks)
         socket.emit('answer', [answer, connection.offerer])
+        console.log(peerConnection)
     }
     console.log(peerConnection)
     peerConnection.addEventListener('icecandidate', event => {
+        console.log(event.candidate)
         if(event.candidate) {
             socket.emit('newICE',event.candidate)
         }
     })
-    peerConnection.addEventListener('track', event => {
+    peerConnection.addEventListener('track', event => { //answerer neeed tracks
         console.log(event)
-        remoteVideo.srcObject = remoteStream
+        remoteVideo.srcObject = event.streams[0]
     })
 }
-
-//i need to set remote description on the offerer browser
 
 socket.on('newConnection', connection=>{
     joinGame.innerHTML = ""
@@ -65,8 +67,11 @@ socket.on('newConnection', connection=>{
     joinGame.appendChild(join)
 })
 
-socket.on('newIceCandidate', candidate => {
-    console.log(candidate)
+socket.on('newIceCandidate', async answer => {
+    if(!peerConnection.remoteDescription){
+        await peerConnection.setRemoteDescription( new RTCSessionDescription(answer[1]))
+    }
+    peerConnection.addIceCandidate(answer[0])
 
 })
 
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const fs = require('fs')
 const https = require('https')
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const socketio = require('socket.io')
 const app = express()
 
 app.use(express.static(__dirname))
+app.use(cors())
 
 const key = fs.readFileSync('cert.key')
 const cert = fs.readFileSync('cert.crt')
 
 const expressServer = https.createServer({cert, key}, app)
-const io = new socketio.Server(expressServer)
+const io = new socketio.Server(expressServer,{
+    cors: {
+        origin: [
+            "https://localhost",
+            'https://192.168.1.72'
+        ]
+    }
+})
 
 let connections = []
 
@@ -25,32 +33,34 @@ io.on('connection', (socket)=> {
             answer: null,
             answererICE: []
         })
+        console.log(offer)
         io.emit('newConnection', connections[connections.length-1])
     })
 
     socket.on('newICE', candidate => {
         let connection = connections.find(element => element.offerer === socket.id)
         if (connection){
+            console.log(connection, "offer")
             connection.offererICE.push(candidate) 
             io.to(connection?.answerer).emit('newIceCandidate', candidate)
             return}
         connection = connections.find(element=>element.answerer === socket.id)
         if (connection){
             connection.answererICE.push(candidate) 
-            console.log(connection)
-            io.to(connection.offerer).emit('newIceCandidate', candidate)
+            console.log(connection, "answer")
+            io.to(connection.offerer).emit('newIceCandidate', [candidate, connection.answer])
             return}
     })
 
     socket.on('answer', answer => {
-        const connection = connections.find(element => element.offerer === answer[1])
+        const connection = connections.find(connection => connection.offerer === answer[1])
         connection.answerer = socket.id
         connection.answer = answer[0]
     })
 
-    socket.on('disconnect', socket=>{
-        console.log(socket,"disconnected")
-    })
+    // socket.on('disconnect', socket=>{
+    //     console.log(socket,"disconnected")
+    // })
 })
 
 
