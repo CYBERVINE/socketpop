a481666a7963353452d4f78e5f1ce5e3861fd9bf
Kevin <kevinspbyrne@gmail.com>
2024-02-12 20:39:13 -0800

    add peerconnection event listner IMMEDIATELY after instance construction so remote stream events are caught

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ const configuration = {
                   'stun:stun1.l.google.com:19302'
 ]}]}
 
+
+const peerConnection = new RTCPeerConnection(configuration)
+
+peerConnection.addEventListener('icecandidate', event => {
+    console.log(event.candidate)
+    if(event.candidate) {
+        socket.emit('newICE',event.candidate)
+    }
+})
+peerConnection.addEventListener('track', event => {
+    remoteVideo.srcObject = event.streams[0]
+})
+
 const getMedia = async () => {
     const media = await navigator.mediaDevices.getUserMedia({
         video: true,
@@ -19,43 +32,24 @@ const getMedia = async () => {
     })
     localStream = new MediaStream(media)
     localVideo.srcObject = localStream
-}
-
-const peerConnection = new RTCPeerConnection(configuration)
-
-const createPeerConnection = async (connection) => {
     localStream.getTracks().forEach(track=>{
         console.log(track)
         peerConnection.addTrack(track,localStream)})
+}
 
-    console.log(connection)
+const createPeerConnection = async (connection) => {
     if (connection === undefined){
         const offer = await peerConnection.createOffer() // SDP
         localDescription = await peerConnection.setLocalDescription(offer)
         socket.emit('offer', offer)
     } else {
-        console.log(connection)// use this socket id to get recent offerer
         await peerConnection.setRemoteDescription(new RTCSessionDescription(connection.offer))
-        const answer = await peerConnection.createAnswer() //SDP
+        const answer = await peerConnection.createAnswer() 
         await peerConnection.setLocalDescription(answer)
-        // remoteVideo.srcObject = peerConnection.MediaStream
-        console.log(connection.tracks)
         const offer = await socket.emitWithAck('answer', [answer, connection.offerer])
         offer.offererICE.forEach(ICE=>peerConnection.addIceCandidate(ICE))
-        console.log(offer)
-        console.log(peerConnection)
     }
-    console.log(peerConnection)
-    peerConnection.addEventListener('icecandidate', event => {
-        console.log(event.candidate)
-        if(event.candidate) {
-            socket.emit('newICE',event.candidate)
-        }
-    })
-    peerConnection.addEventListener('track', event => { //answerer neeed tracks
-        console.log(event)
-        remoteVideo.srcObject = event.streams[0]
-    })
+
 }
 
 socket.on('newConnection', connection=>{
@@ -80,3 +74,139 @@ socket.on('newIceCandidate', async answer => {
 startGame.addEventListener('click', ()=>createPeerConnection())
 
 getMedia()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const socket = io('https://localhost:8000/')
+// // const socket = io('https://192.168.1.72:8000/')
+// const localVideo = document.getElementById('localVideo')
+// const remoteVideo = document.getElementById('remoteVideo')
+// const startGame = document.getElementById('startGame')
+// const joinGame = document.getElementById('joinGame')
+// let playerOne, playerTwo, remoteStream, localStream, localDescription, offerer
+
+// const configuration = {
+//     'iceServers': [
+//         {'urls': ['stun:stun.l.google.com:19302',
+//                   'stun:stun1.l.google.com:19302',
+//                   "stun:stun2.l.google.com:19302",
+//                     "stun:stun3.l.google.com:19302",
+//                     "stun:stun4.l.google.com:19302",
+//                     "stun:stun01.sipphone.com",
+//                     "stun:stun.ekiga.net",
+//                     "stun:stun.fwdnet.net",
+//                     "stun:stun.ideasip.com",
+// ]}]}
+
+// const peerConnection = new RTCPeerConnection(configuration)
+
+// const getMedia = () => {
+//     return new Promise(async(resolve, reject)=>{
+//         try{
+//     const media = await navigator.mediaDevices.getUserMedia({
+//         video: true,
+//         // audio: false
+//     })
+//         localVideo.srcObject = media
+//         localStream = media
+//         localStream.getTracks().forEach(track=>{
+//             console.log(track)
+//             peerConnection.addTrack(track,localStream)})
+//         resolve();    
+//     }catch(err){
+//         console.log(err);
+//         reject()
+//     }})
+// }
+
+// const createPeerConnection = async (connection) => {
+//    return new Promise(async(resolve, reject)=>{
+//     remoteStream = new MediaStream()
+//     remoteVideo.srcObject = remoteStream
+//     peerConnection.addEventListener('icecandidate', event => {
+//         console.log(event.candidate)
+//         if(event.candidate) {
+//             socket.emit('newICE',event.candidate)
+//         }
+//     })
+//     peerConnection.addEventListener('track', event => { // answerer neeed tracks
+//         console.log(event)
+//         remoteVideo.srcObject = event.streams[0]
+//     })
+
+//     console.log(connection)
+
+//     if (connection === undefined){
+//         const offer = await peerConnection.createOffer()
+//         localDescription = await peerConnection.setLocalDescription(offer)
+//         socket.emit('offer', offer)
+//         console.log(peerConnection.getTransceivers())
+//     } else {
+//         console.log(connection)
+//         await peerConnection.setRemoteDescription(new RTCSessionDescription(connection.offer))
+//         const answer = await peerConnection.createAnswer({}) //this object
+//         const offer = await socket.emitWithAck('answer', [answer, connection.offerer])
+//         offer.offererICE.forEach(ICE=>peerConnection.addIceCandidate(ICE))
+//         await peerConnection.setLocalDescription(answer)
+//         console.log(connection.tracks)
+//         console.log(offer)
+//         console.log(peerConnection)
+//     }
+
+//     // peerConnection.addEventListener('icecandidate', event => {
+//     //     console.log(event.candidate)
+//     //     if(event.candidate) {
+//     //         socket.emit('newICE',event.candidate)
+//     //     }
+//     // })
+//     // peerConnection.addEventListener('track', event => { // answerer neeed tracks
+//     //     console.log(event)
+//     //     remoteVideo.srcObject = event.streams[0]
+//     // })
+//     resolve();
+// })
+// }
+
+// const makeCall = async ()=> {
+//     await getMedia()
+//     await createPeerConnection()
+// }
+
+// const answerCall = async (connection)=>{
+//     await getMedia()
+//     await createPeerConnection(connection)
+// }
+
+// socket.on('newConnection', connection=>{
+//     joinGame.innerHTML = ""
+//     const join = document.createElement("button")
+//     if (socket.id === connection.offerer )  join.innerText = "game request made" 
+//     else {
+//         join.addEventListener('click', ()=> answerCall(connection))
+//         join.innerText = `click to play ${connection.offerer}`
+//     }
+//     joinGame.appendChild(join)
+// })
+
+// socket.on('newIceCandidate', async answer => {
+//     if(!peerConnection.remoteDescription){
+//         await peerConnection.setRemoteDescription( new RTCSessionDescription(answer[1]))
+//     }
+//     peerConnection.addIceCandidate(answer[0])
+// })
+
+// startGame.addEventListener('click', ()=>makeCall())
